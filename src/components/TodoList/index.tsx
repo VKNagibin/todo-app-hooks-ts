43ba2815a742958b22cf0012ca4f4d6496cc2c35
VKNagibin <itisnotstupid@gmail.com>
2022-08-16 11:11:57 +0300

    refactoring of TodoList component

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,29 +1,19 @@
 import ListItem from "../ListItem";
-import React, { useCallback } from "react";
+import React from "react";
 import { Button } from "../CreateTodo/styled";
 import { Container } from "./styled";
 import IPropsTodoList from "./types";
 import {memo} from "react";
 
 const TodoList = (props: IPropsTodoList): JSX.Element | null => {
-    const deleteHandler = useCallback((id: string) => props.handleDelete(id) ,[props.handleDelete]);
-
-    const deleteMarkedHandler = () => props.handleDeleteMarked();
-
-    const checkboxHandler = (id: string) => props.handleCheckbox(id);
-
-    const editHandler = (id: string, newContent: string) => props.handleEdit(id, newContent);
-
-    const markAllHandler = () => props.handleMarkAll();
-
     return (
         !props.arrayTodos.length ? null :
     <Container>
         {
             props.arrayTodos.map(item => (
-                <ListItem deleteHandler={deleteHandler}
-                       editHandler={editHandler}
-                       checkboxHandler={checkboxHandler}
+                <ListItem deleteHandler={props.handleDelete}
+                       editHandler={props.handleEdit}
+                       checkboxHandler={props.handleCheckbox}
                        content={item.content}
                        key={item.id}
                        id={item.id}
@@ -31,8 +21,8 @@ const TodoList = (props: IPropsTodoList): JSX.Element | null => {
                 />
             ))
         }
-        <Button onClick={deleteMarkedHandler}>Delete marked</Button>
-        <Button onClick={markAllHandler}>Mark all</Button>
+        <Button onClick={props.handleDeleteMarked}>Delete marked</Button>
+        <Button onClick={props.handleMarkAll}>Mark all</Button>
     </Container>
     )
 }
